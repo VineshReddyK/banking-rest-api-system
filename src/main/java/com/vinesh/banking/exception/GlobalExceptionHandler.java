57a18c2f8c66247d8d8cbc6247f096f1ba3b4f66
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:46:48 -0500

    Add global exception handler

--- src/main/java/com/vinesh/banking/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vinesh/banking/exception/GlobalExceptionHandler.java
@@ -1,2 +1,20 @@
+package com.vinesh.banking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception exception) {
+        return new ResponseEntity<>("Something went wrong: " + exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
