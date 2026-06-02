902fb0ce1a1ee86b687db41f0977f9c6f76fe6e2
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:34:18 -0500

    Update AuthController.java

--- src/main/java/com/vinesh/banking/controller/AuthController.java
+++ src/main/java/com/vinesh/banking/controller/AuthController.java
@@ -6,4 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+     @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return "User registered successfully";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return "Login successful";
+    }
 }
