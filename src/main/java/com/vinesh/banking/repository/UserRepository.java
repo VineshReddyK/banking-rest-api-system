16a9ce0d951a7e8b36f9d5a510994c1505047116
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:28:58 -0500

    Update UserRepository.java

--- src/main/java/com/vinesh/banking/repository/UserRepository.java
+++ src/main/java/com/vinesh/banking/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.vinesh.banking.repository;
 import com.vinesh.banking.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
