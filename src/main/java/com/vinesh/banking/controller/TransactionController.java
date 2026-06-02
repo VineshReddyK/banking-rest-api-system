76a90f56bdd99ed9d0559a99ef726d4d860a2950
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 14:32:27 -0500

    Update TransactionController.java

--- src/main/java/com/vinesh/banking/controller/TransactionController.java
+++ src/main/java/com/vinesh/banking/controller/TransactionController.java
@@ -1,4 +1,8 @@
 package com.vinesh.banking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
