8b4627900a313dc6045d290dc09af68f3b25ed49
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 15:07:45 -0500

    Connect AccountController to AccountService

--- src/main/java/com/vinesh/banking/controller/AccountController.java
+++ src/main/java/com/vinesh/banking/controller/AccountController.java
@@ -1,19 +1,24 @@
 package com.vinesh.banking.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vinesh.banking.dto.AccountRequest;
+import com.vinesh.banking.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
-  
+
+    @Autowired
+    private AccountService accountService;
+
     @PostMapping
     public String createAccount(@RequestBody AccountRequest request) {
-        return "Account created successfully";
+        return accountService.createAccount();
     }
 
     @GetMapping
     public String getAccounts() {
-        return "Accounts fetched successfully";
+        return accountService.getAccounts();
     }
 }
