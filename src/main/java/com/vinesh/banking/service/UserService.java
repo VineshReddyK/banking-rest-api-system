c0185ee40cd97dbe27f451b2361cd96f96cb3d0b
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:30:20 -0500

    Update UserService.java

--- src/main/java/com/vinesh/banking/service/UserService.java
+++ src/main/java/com/vinesh/banking/service/UserService.java
@@ -1,5 +1,12 @@
 package com.vinesh.banking.service;
 
+import com.vinesh.banking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
 }
