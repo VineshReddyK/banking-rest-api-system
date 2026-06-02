e0923f1c5961819cc8e1f2f7b8e0c1cc78426b9f
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:31:08 -0500

    Update TransactionService.java

--- src/main/java/com/vinesh/banking/service/TransactionService.java
+++ src/main/java/com/vinesh/banking/service/TransactionService.java
@@ -1,4 +1,12 @@
 package com.vinesh.banking.service;
+
+import com.vinesh.banking.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
 }
