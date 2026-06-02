92be67c44fc19f5240001f71f1d2bcc1d6990add
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 14:38:08 -0500

    Update AccountRequest.java

--- src/main/java/com/vinesh/banking/dto/AccountRequest.java
+++ src/main/java/com/vinesh/banking/dto/AccountRequest.java
@@ -5,4 +5,22 @@ public class AccountRequest {
     private String accountType;
     private Double initialDeposit;
 
+    public AccountRequest() {
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Double getInitialDeposit() {
+        return initialDeposit;
+    }
+
+    public void setInitialDeposit(Double initialDeposit) {
+        this.initialDeposit = initialDeposit;
+    }
 }
