f8bddaa6edc9b5ca4bff5786aaa82a57fcadae32
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 11:31:23 -0500

    Create Transaction entity

---
+++ src/main/java/com/vinesh/banking/entity/Transaction.java
@@ -0,0 +1,12 @@
+package com.vinesh.banking.entity;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+
+    private Long id;
+    private Double amount;
+    private String type;
+    private LocalDateTime transactionDate;
+
+}
