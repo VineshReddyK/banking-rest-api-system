e1b43fd57a81a81cfaf8c74073a6883d13c50777
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:35:13 -0500

    Update TransactionController.java

--- src/main/java/com/vinesh/banking/controller/TransactionController.java
+++ src/main/java/com/vinesh/banking/controller/TransactionController.java
@@ -6,4 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
+  @PostMapping("/deposit")
+    public String deposit(@RequestBody TransactionRequest request) {
+        return "Deposit successful";
+    }
+
+    @PostMapping("/withdraw")
+    public String withdraw(@RequestBody TransactionRequest request) {
+        return "Withdrawal successful";
+    }
+
+    @PostMapping("/transfer")
+    public String transfer(@RequestBody TransactionRequest request) {
+        return "Transfer successful";
+    }
 }
