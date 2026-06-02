e922f19b8e7d60780e7bea97c0593ba3d3442bdd
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 14:30:44 -0500

    Update AccountService.java

--- src/main/java/com/vinesh/banking/service/AccountService.java
+++ src/main/java/com/vinesh/banking/service/AccountService.java
@@ -1,4 +1,12 @@
 package com.vinesh.banking.service;
+
+import com.vinesh.banking.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
 }
