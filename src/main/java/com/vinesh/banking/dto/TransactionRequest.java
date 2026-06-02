16c1a9d7de6a19ebff48f95c5229f07c9b2aa7ba
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 12:07:00 -0500

    Update TransactionRequest.java

--- src/main/java/com/vinesh/banking/dto/TransactionRequest.java
+++ src/main/java/com/vinesh/banking/dto/TransactionRequest.java
@@ -1,1 +1,9 @@
-#placeholder
+package com.vinesh.banking.dto;
+
+public class TransactionRequest {
+
+    private Long accountId;
+    private Double amount;
+    private String transactionType;
+
+}
