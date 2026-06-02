82692a825f22374dd82cd9b23971ceb436d1230e
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 15:08:16 -0500

    Connect TransactionController to TransactionService

--- src/main/java/com/vinesh/banking/controller/TransactionController.java
+++ src/main/java/com/vinesh/banking/controller/TransactionController.java
@@ -1,23 +1,29 @@
 package com.vinesh.banking.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vinesh.banking.dto.TransactionRequest;
+import com.vinesh.banking.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
-  @PostMapping("/deposit")
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping("/deposit")
     public String deposit(@RequestBody TransactionRequest request) {
-        return "Deposit successful";
+        return transactionService.deposit();
     }
 
     @PostMapping("/withdraw")
     public String withdraw(@RequestBody TransactionRequest request) {
-        return "Withdrawal successful";
+        return transactionService.withdraw();
     }
 
     @PostMapping("/transfer")
     public String transfer(@RequestBody TransactionRequest request) {
-        return "Transfer successful";
+        return transactionService.transfer();
     }
 }
