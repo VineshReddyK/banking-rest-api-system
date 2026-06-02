a91fb33b17e82bf9b6cf66ab875a82297ce97962
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 14:22:09 -0500

    Update UserRepository.java

--- src/main/java/com/vinesh/banking/repository/UserRepository.java
+++ src/main/java/com/vinesh/banking/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.vinesh.banking.repository;
 
-public interface UserRepository {
+import com.vinesh.banking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface UserRepository extends JpaRepository<User, Long> {
 }
