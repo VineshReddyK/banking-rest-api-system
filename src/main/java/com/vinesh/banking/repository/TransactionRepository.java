be81df3ac8baf10125bb81b22b1badce12ef16f2
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:29:38 -0500

    Update TransactionRepository.java

--- src/main/java/com/vinesh/banking/repository/TransactionRepository.java
+++ src/main/java/com/vinesh/banking/repository/TransactionRepository.java
@@ -1,2 +1,7 @@
+package com.vinesh.banking.repository;
+
+import com.vinesh.banking.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
