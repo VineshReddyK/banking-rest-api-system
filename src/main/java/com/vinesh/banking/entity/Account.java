74594e952c4cb4535f2c2fbf9b9372dbdc500b63
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 14:41:10 -0500

    Add Account entity getters and setters

--- src/main/java/com/vinesh/banking/entity/Account.java
+++ src/main/java/com/vinesh/banking/entity/Account.java
@@ -8,4 +8,27 @@ public class Account {
 
     private String accountNumber;
     private Double balance;
+    public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public String getAccountNumber() {
+    return accountNumber;
+}
+
+public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+}
+
+public Double getBalance() {
+    return balance;
+}
+
+public void setBalance(Double balance) {
+    this.balance = balance;
+}
 }
