54b4192d7292e9b31ae037570252c77330020292
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 14:42:05 -0500

    Add Transaction entity getters and setters

--- src/main/java/com/vinesh/banking/entity/Transaction.java
+++ src/main/java/com/vinesh/banking/entity/Transaction.java
@@ -9,4 +9,35 @@ public class Transaction {
    private Double amount;
 private String transactionType;
 private LocalDateTime transactionDate;
+    public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public Double getAmount() {
+    return amount;
+}
+
+public void setAmount(Double amount) {
+    this.amount = amount;
+}
+
+public String getTransactionType() {
+    return transactionType;
+}
+
+public void setTransactionType(String transactionType) {
+    this.transactionType = transactionType;
+}
+
+public LocalDateTime getTransactionDate() {
+    return transactionDate;
+}
+
+public void setTransactionDate(LocalDateTime transactionDate) {
+    this.transactionDate = transactionDate;
+}
 }
