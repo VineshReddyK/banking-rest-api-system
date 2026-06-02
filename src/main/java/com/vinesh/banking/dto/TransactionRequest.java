fac9b4b5d55f55ce90819f25662922cdd34d026b
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 14:38:29 -0500

    Update TransactionRequest.java

--- src/main/java/com/vinesh/banking/dto/TransactionRequest.java
+++ src/main/java/com/vinesh/banking/dto/TransactionRequest.java
@@ -3,7 +3,42 @@ package com.vinesh.banking.dto;
 public class TransactionRequest {
 
     private Long accountId;
+    private Long targetAccountId;
     private Double amount;
     private String transactionType;
 
+    public TransactionRequest() {
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Long getTargetAccountId() {
+        return targetAccountId;
+    }
+
+    public void setTargetAccountId(Long targetAccountId) {
+        this.targetAccountId = targetAccountId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
 }
