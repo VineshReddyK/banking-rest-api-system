08a1ca4f002e928f0490775ab3ea906e7bd04c3d
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:47:33 -0500

    Update JwtUtil.java

--- src/main/java/com/vinesh/banking/security/JwtUtil.java
+++ src/main/java/com/vinesh/banking/security/JwtUtil.java
@@ -1,2 +1,11 @@
+package com.vinesh.banking.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
+
+    public String generateToken(String email) {
+        return "sample-jwt-token-for-" + email;
+    }
 }
