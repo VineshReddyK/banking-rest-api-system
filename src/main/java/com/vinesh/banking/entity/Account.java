1267e70643045325380fb59553a6068611a76a8b
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-02 14:16:05 -0500

    Update Account.java

--- src/main/java/com/vinesh/banking/entity/Account.java
+++ src/main/java/com/vinesh/banking/entity/Account.java
@@ -1,9 +1,11 @@
-package com.vinesh.banking.entity;
-
+@Entity
+@Table(name = "accounts")
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String accountNumber;
     private Double balance;
-
 }
