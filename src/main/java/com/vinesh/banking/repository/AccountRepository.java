d5abe1214e262d2e84079cafa4c34182fdf09148
Vinesh Reddy Kankanalapally <164949271+VineshReddyK@users.noreply.github.com>
2026-06-02 14:29:15 -0500

    Update AccountRepository.java

--- src/main/java/com/vinesh/banking/repository/AccountRepository.java
+++ src/main/java/com/vinesh/banking/repository/AccountRepository.java
@@ -1,2 +1,7 @@
+package com.vinesh.banking.repository;
+
+import com.vinesh.banking.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
 }
