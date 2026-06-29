b321129da9b3da0a3e6a51e60f4fe2b1c244431c
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-29 13:03:08 -0500

    Improved version of AuthController

--- src/main/java/com/vinesh/banking/controller/AuthController.java
+++ src/main/java/com/vinesh/banking/controller/AuthController.java
@@ -4,7 +4,7 @@ import com.vinesh.banking.dto.LoginRequest;
 import com.vinesh.banking.dto.LoginResponse;
 import com.vinesh.banking.dto.RegisterRequest;
 import com.vinesh.banking.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,17 +13,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@jakarta.validation.Valid @RequestBody RegisterRequest request) {
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
         String message = userService.registerUser(request);
-        return new ResponseEntity<>(message, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(message);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@jakarta.validation.Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
         LoginResponse response = userService.loginUser(request);
         return ResponseEntity.ok(response);
     }
