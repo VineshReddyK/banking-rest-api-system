25e2f5c4e3de058489bc1cbcb3faa0ad50301d48
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-29 13:03:15 -0500

    Improved version of LoginResponse

--- src/main/java/com/vinesh/banking/dto/LoginResponse.java
+++ src/main/java/com/vinesh/banking/dto/LoginResponse.java
@@ -4,8 +4,18 @@ public class LoginResponse {
 
     private String token;
 
-    public LoginResponse() {}
+    public LoginResponse() {
+    }
 
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
