618fae388b8ffc20bf00293c89c26e6538c4b3c3
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-29 13:03:35 -0500

    Improved version of AccountRepository

--- src/main/java/com/vinesh/banking/repository/AccountRepository.java
+++ src/main/java/com/vinesh/banking/repository/AccountRepository.java
@@ -1,7 +1,12 @@
 package com.vinesh.banking.repository;
 
 import com.vinesh.banking.entity.Account;
+import com.vinesh.banking.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    List<Account> findByUser(User user);
 }
