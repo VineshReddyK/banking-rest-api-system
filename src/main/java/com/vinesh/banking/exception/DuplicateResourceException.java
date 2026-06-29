3faec7b2038974738d369467295c1ffcdf4423de
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-29 13:03:25 -0500

    Improved version of DuplicateResourceException

--- src/main/java/com/vinesh/banking/exception/DuplicateResourceException.java
+++ src/main/java/com/vinesh/banking/exception/DuplicateResourceException.java
@@ -2,6 +2,8 @@ package com.vinesh.banking.exception;
 
 public class DuplicateResourceException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public DuplicateResourceException(String message) {
         super(message);
     }
