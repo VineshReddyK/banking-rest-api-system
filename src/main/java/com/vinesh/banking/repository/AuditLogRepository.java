8f8981e9d81543823f4cb12d9b1f3b26f22a8940
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-06-29 13:03:36 -0500

    Improved version of AuditLogRepository

--- src/main/java/com/vinesh/banking/repository/AuditLogRepository.java
+++ src/main/java/com/vinesh/banking/repository/AuditLogRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+
     List<AuditLog> findByPerformedByOrderByTimestampDesc(String performedBy);
 }
